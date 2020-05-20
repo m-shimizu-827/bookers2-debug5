d5958588f5b1b7fb7307eaa10eb658de8aeef3d1
mst827 <k.s.m501196gmail.com>
2020-05-20 17:09:36 +0900

    finish Task4

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,28 @@ class User < ApplicationRecord
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  has_many :favorites
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+
+  end
+
+  def unfollow(other_user)
+    relationship =self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about' =>'home#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update,:create,:destroy]
-  resources :books,only: [:index,:edit,:show,:update,:create,:destroy]
+  resources :users,only: [:show,:index,:edit,:update,:create,:destroy] do
+  	member do
+  		get :following, :followers
+  	end
+  end
+  resources :books,only: [:index,:edit,:show,:update,:create,:destroy] do
+  	resource :favorites, only:[:create, :destroy]
+  	resource :book_comments, only: [:create, :destroy]
+  end
+  resources :relationships, only: [:create, :destroy]
+
   
   
 end
